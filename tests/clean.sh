3fdb8a06da87ee0f9cae8340fba8ad7fe9227aca
Paulo Henrique Silva <pH@Godel.local>
2010-11-30 23:44:30 -0200

    Add a compressed template image on test-image test.

--- tests/clean.sh
+++ tests/clean.sh
@@ -2,5 +2,6 @@
 
 TEST_DIR=`dirname $0`
 
+rm -f $TEST_DIR/test_image.pgm.template
 rm -f $TEST_DIR/__autogen__*.pgm
 
--- tests/run.sh
+++ tests/run.sh
@@ -5,5 +5,7 @@ TEST_DIR=`dirname $0`
 export LD_LIBRARY_PATH=$TEST_DIR/../libjp4
 export DYLD_LIBRARY_PATH=$TEST_DIR/../libjp4
 
+bzcat -f $TEST_DIR/test_image.pgm.template.bz2 > $TEST_DIR/test_image.pgm.template
+
 $TEST_DIR/test_image
 
